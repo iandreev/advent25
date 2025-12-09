3960a1979b51fe3165af417ae9b8f2416a5564bd
Ivan Andreev <vankaandreev@gmail.com>
2025-12-09 22:59:45 +0100

    day3

---
+++ src/Advent25/Days/Day3.cs
@@ -0,0 +1,50 @@
+﻿namespace Advent25.Days;
+
+internal class Day3
+{
+    internal void Solve1()
+    {
+        var lines = GetInput();
+
+        long result = 0;
+        foreach (var line in lines)
+        {
+            var best = FindBest(line, 2);
+            result += long.Parse(best);
+        }
+
+        Console.WriteLine(result);
+    }
+
+    string FindBest(string line, int digits)
+    {
+        if (digits == 0) return "";
+        var max = line.ToCharArray().Take(line.Length - digits + 1).Max();
+        var firstIndex = line.IndexOf(max);
+
+        var sub = FindBest(line.Substring(firstIndex + 1), digits - 1);
+
+        return max.ToString() + sub;
+    }
+
+    internal void Solve2()
+    {
+        var lines = GetInput2();
+
+        long result = 0;
+        foreach (var line in lines)
+        {
+            var best = FindBest(line, 12);
+            result += long.Parse(best);
+        }
+
+        Console.WriteLine(result);
+    }
+
+
+    List<string> GetInput() => File.ReadAllLines("Data/day3.txt")
+        .ToList();
+
+    List<string> GetInput2() => File.ReadAllLines("Data/day3_part2.txt")
+        .ToList();
+}
--- src/Advent25/Program.cs
+++ src/Advent25/Program.cs
@@ -3,5 +3,8 @@
 //new Day1().Solve1();
 //new Day1().Solve2();
 
-new Day2().Solve1();
-new Day2().Solve2();
+//new Day2().Solve1();
+//new Day2().Solve2();
+
+new Day3().Solve1();
+new Day3().Solve2();
